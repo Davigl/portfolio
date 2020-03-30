790d11c605cb9f4270a26d737636d9cb30267645
Davi <davi.guimaraesleite@gmail.com>
2020-03-30 20:29:21 -0300

    fix: home responsiveness.

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
+import { device } from '~/assets/styles/partials/_variables';
+
 export const Footer = styled.footer`
   position: relative;
   bottom: 10px;
   margin-left: 20px;
+
+  @media ${device.tablet} {
+    text-align: center;
+    margin-left: 0;
+  }
 `;
 
 export const FooterText = styled.p`
   font-family: 'NeurialGrotesk-Regular';
-  color: #666;
+  color: #999;
   font-size: 12px;
 `;
